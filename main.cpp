0c54b23fbaac011a111ea6ce921e21430770ea7a
Steffo <bjoekeldude@gmail.com>
2021-11-18 19:47:59 +0100

    initial commit

---
+++ main.cpp
@@ -0,0 +1,58 @@
+/**	\file main.cpp
+*	\author Stephan Bökelmann
+*	\brief This Program provides awesome functionality
+*	
+*	With its brilliant architecture it's the best Program in the World
+*	
+*	\bug Never ever had any. Otherwise look at https://github.com/bjoekeldude
+*/
+
+#include<iostream>
+#include<vector>
+#include<string>
+
+/** 
+ * 	\struct meineKlasse 
+ * 	\brief Encapsulates some variables
+ * 	\var int $meineKlasse::a
+ * 	Member 'a' hat folgende Funktion....
+ * 	\var in $meineKlasse::b
+ * 	Während 'b' etwas anderes zutun hat 
+ */
+struct meineKlasse(){
+	int a;
+	int b;
+}
+
+/**	\brief Sums up two integers
+ *
+ * 	Uses \latexonly $\frac{3}{2}$\endlatexonly to calculate something
+ *
+ * 	\param Summand number one
+ * 	\param Summand number two
+ *
+ * 	\return integer
+ */
+int rechne_zusammen(int a, int b){
+	/*
+	 * This sums up something
+	 */
+	return a+b;
+}
+
+
+int main(){
+	/*
+	 * Make a Vector of strings
+	 */
+    std::vector<std::string> msg{"Hello", "THGA", "World","!"};
+    	/*
+	 * Print out everything since we want to see it on the screen
+	 * Refers to Page 143 of specification
+	 */
+    for(char& letter : file){
+        if(letter == 'a') a++;
+    }
+
+    return 0;
+}
